ee26f621af7aac0cc46c5dde3e42d32b62d2b624
ysh <ysh@et.cn>
2024-01-19 18:00:56 +0800

    提交一版代码

--- demo/api/demo/internal/logic/demologic.go
+++ demo/api/demo/internal/logic/demologic.go
@@ -2,6 +2,7 @@ package logic
 
 import (
 	"context"
+	"go-zero-blog/demo/rpc/demo/demo"
 
 	"go-zero-blog/demo/api/demo/internal/svc"
 	"go-zero-blog/demo/api/demo/internal/types"
@@ -26,5 +27,8 @@ func NewDemoLogic(ctx context.Context, svcCtx *svc.ServiceContext) *DemoLogic {
 func (l *DemoLogic) Demo(req *types.Request) (resp *types.Response, err error) {
 	// todo: add your logic here and delete this line
 
-	return
+	l.svcCtx.DemoRpc.Ping(l.ctx, &demo.Request{
+		Ping: req.Name,
+	})
+	return resp, nil
 }
--- demo/api/demo/internal/svc/servicecontext.go
+++ demo/api/demo/internal/svc/servicecontext.go
@@ -1,15 +1,19 @@
 package svc
 
 import (
+	"github.com/zeromicro/go-zero/zrpc"
 	"go-zero-blog/demo/api/demo/internal/config"
+	"go-zero-blog/demo/rpc/demo/democlient"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config  config.Config
+	DemoRpc democlient.Demo
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
-		Config: c,
+		Config:  c,
+		DemoRpc: democlient.NewDemo(zrpc.MustNewClient(c.DemoRpcConf)),
 	}
 }
