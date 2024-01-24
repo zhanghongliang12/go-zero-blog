b2b659b901772aad9d26065174380725cec0605a
ysh <ysh@et.cn>
2024-01-24 15:11:59 +0800

    提交第一版代码

---
+++ apis/user/api/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"go-zero-blog/apis/user/api/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
